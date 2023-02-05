cb3b33a0d43848a7a6bf144b4712d94232cb6c2b
OksanaV2284 <110124340+OksanaV2284@users.noreply.github.com>
2023-02-05 12:33:56 +0200

    стилі

---
+++ src/js/modal.js
@@ -0,0 +1,17 @@
+// відкриває модалку по кліку на кнопку і закриває ,
+//не зекриває по кліку на бекдроп
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
